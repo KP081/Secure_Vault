c57c197456c747fe4338c1acdb1687470b09dd5c
kathan <kathan.ptlkathan1@gmail.com>
2025-07-02 09:13:53 +0000

    change solidity version according slither analysis

--- contracts/SecureVault.sol
+++ contracts/SecureVault.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
 import "node_modules/@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
@@ -19,5 +19,5 @@ contract SecureVault is ReentrancyGuard {
         (bool success , ) = msg.sender.call{value : amount}("");
         require(success , "Transfer failed");
     }
-    
+
 }
